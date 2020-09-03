3742f59fb15b1b5ed85bdf2a95c04520a649cacb
周杰 <1342134929@qq.com>
2020-09-03 09:52:25 +0800

    ele按需引入

--- src/main.js
+++ src/main.js
@@ -3,9 +3,34 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+// Vue.use(ElementUI)
+
+import {
+  Button,
+  ButtonGroup,
+  Table,
+  TableColumn,
+  Pagination,
+  Menu,
+  MenuItem,
+  MenuItemGroup,
+  Tooltip,
+  Submenu,
+  Loading,
+  MessageBox,
+  Message,
+} from 'element-ui';
+
+Vue.use(Button).use(ButtonGroup).use(Table).use(TableColumn).use(Pagination).use(Menu).use(MenuItem).use(MenuItemGroup).use(Tooltip).use(Submenu)
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
 
 import animate from "animate.css";
 Vue.use(animate);
